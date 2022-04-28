d127b0b1438821272dbcdbd7b5cb6064d3c48414
root <root@localhost.localdomain>
2022-04-28 15:14:19 +0500

    commit#5

--- environments/production/manifests/main.pp
+++ environments/production/manifests/main.pp
@@ -3,6 +3,7 @@ node 'serverdebain02-vmwarevirtualplatform.localdomain.lan' {
 # include serverdebain02
   include lemp
   include sample
+  include zabbix
 # include treydock-keycloak
 #  include 'treydock-keycloak', '7.17.0'
 #  include treydock-keycloak
